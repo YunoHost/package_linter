1d56ef55c0be0f6e6ad4691af75afc63328ac7d7
Mickael <root@mike.test>
2019-02-25 21:00:12 +0100

    add exclusion for source cmd files called by source can be not present on a dev environment

--- package_linter.py
+++ package_linter.py
@@ -440,7 +440,7 @@ def check_deprecated_practices(script):
 
 def check_shellcheck(script_name):
     try: 
-        subprocess.run(["shellcheck","-x",script_name])
+        subprocess.run(["shellcheck","-e","SC1091","-x",script_name])
     except FileNotFoundError as err:
         print_warning("Binary shellcheck not found in $PATH, try to install it")
         print_warning("Cannot parse the script with shellcheck")
