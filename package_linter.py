677d48f50f6bdda4ea16dd31efb572371b4ba782
Alexandre Aubin <alex.aubin@mailoo.org>
2024-09-28 19:26:58 +0200

    misc: update/simplify URLs displayed in intro, remove reference to experimetal helpers which are basically unused / integrated in the core now

--- package_linter.py
+++ package_linter.py
@@ -22,19 +22,15 @@ def main():
         set_output_json()
 
     _print(
-        """
-    [{blue}{bold}YunoHost App Package Linter{end}]
+        """    [YunoHost App Package Linter]
 
- App packaging documentation - https://yunohost.org/en/contribute/packaging_apps
+ App packaging documentation - https://yunohost.org/packaging_apps
  App package example         - https://github.com/YunoHost/example_ynh
- Official helpers            - https://yunohost.org/en/contribute/packaging_apps/helpers
- Experimental helpers        - https://github.com/YunoHost-Apps/Experimental_helpers
+ Official helpers            - https://yunohost.org/packaging_apps_helpers
 
  If you believe this linter returns false negative (warnings / errors which shouldn't happen),
  please report them on https://github.com/YunoHost/package_linter/issues
-    """.format(
-            blue=c.OKBLUE, bold=c.BOLD, end=c.END
-        )
+    """
     )
 
     app = App(str(args.app_path))
