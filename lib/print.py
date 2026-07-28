21da7a9c6d2c4ef60f6e3e44fa27892dbf68e197
Félix Piédallu <felix@piedallu.me>
2026-07-28 17:10:41 +0200

    lib: Use functools.wraps for _print
    This helps with type hinting.

--- lib/print.py
+++ lib/print.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 
-from typing import Any
+from functools import wraps
 
 output = "plain"
 
 
-def _print(*args: Any, **kwargs: Any) -> None:
+@wraps(print)
+def _print(*values: object, **kwargs) -> None:  # noqa: ANN003
     if not is_json_output():
-        print(*args, **kwargs)
+        print(*values, **kwargs)
 
 
 def set_output_json() -> None:
